d71cbbbd5f05375b97e4fea531abcbb25bf076cb
Tuhin Reza <tuhinreza02042002@gmail.com>
2026-04-14 00:14:40 +0600

    Environment variable added

--- MainProject/Assignment-6/vite.config.js
+++ MainProject/Assignment-6/vite.config.js
@@ -3,4 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  // Remove the complex base path.
+  // '/' is the default and works for almost all Vercel deployments.
+  base: "/",
 });
